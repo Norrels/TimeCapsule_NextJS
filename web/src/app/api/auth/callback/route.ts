7f7e3a53bef6a007d8da811bfc0a0cb745fd86c1
Matheus Zacarias <matheuus412@gmail.com>
2023-05-21 02:24:31 -0300

    :sparkles: Autenticando o usuário

---
+++ web/src/app/api/auth/callback/route.ts
@@ -0,0 +1,27 @@
+import { api } from "@/lib/api";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url);
+  const code = searchParams.get("code");
+
+  const registerResponse = await api.post("/register", {
+    code,
+  });
+
+  const { token } = registerResponse.data;
+
+  // é possivel saber de que url a req foi feita,
+  // Nesse caso como só em uma url especeficia essa chamada pode ser feita (home) vamos redireciona o usuário para ela
+  const redirectURL = new URL("/", request.url);
+
+  const cookiesExpiresInSeconds = 60 * 60 * 24 * 30; // 30 dias
+
+  return NextResponse.redirect(redirectURL, {
+    // Esse path no final significa que esse cookie vai estar disponivel em todas as rotas da aplicação
+    // Caso eu quise limitar isso seria path=/auth por exemplo
+    headers: {
+      "Set-Cookie": `token=${token}; Path=/; max-age=${cookiesExpiresInSeconds}`,
+    },
+  });
+}
